1ea0ffb6da3ddef2195e13c08e52958d167e0601
CristianAC7 <cristian.emdina-m@mail.escuelaing.edu.co>
2015-11-12 12:07:58 -0500

    Parte II

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -1,6 +1,44 @@
 (function () {
     var app = angular.module('modone', []);
+    var cnv = document.getElementById("myCanvas");
+//    var ctx = cnv.getContext("2d");
+    app.controller("plan_control", function ($scope, $log, $http) {
+        $scope.entry = {Figura: "Nombre"};
+        $log.debug('se creo el $scope');
+        $scope.entries = [];
+
+        $scope.loadData = function () {
+            var configList = {
+                method: "GET",
+                url: "blueprints"
+            };
+            var response = $http(configList);
+            response.success(function (data, status, headers, config) {
+                $scope.entries = data;
+            });
+            response.error(function (data, status, headers, config) {
+                alert("The petition has failed. HTTP Status:" + status);
+            });
+        };
+        $scope.loadData();
+
+        $scope.getCanvas = function (item) {
+            var configList = {
+                method: "GET",
+                url: "blueprints/"+$scope.Figura
+            };
+            var response = $http(configList);
+            response.success(function (data, status, headers, config) {
+                $scope.entries = data;
+            });
+            response.error(function (data, status, headers, config) {
+                alert("The petition has failed. HTTP Status:" + status);
+            });
+        };
+
+    });
 })();
+
 
 
 
